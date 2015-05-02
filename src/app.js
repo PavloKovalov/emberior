ef14564cb12c382aa3fbecaa55c5b54d6d145c3a
Pavel Kovalyov <sxe.funky@gmail.com>
2015-05-02 15:56:55 +0300

    Added products route and controller

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
-/**
- * Created by pavel on 5/1/15.
- */
+/* global Ember */
+'use strict';
+
 var App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
 App.Router.map(function() {
     this.route('about');
+    this.resource('products');
 });
+
+App.ProductsRoute = Ember.Route.extend({
+    model: function() {
+        return App.PRODUCTS;
+    }
+});
+
+App.PRODUCTS = [
+    {
+        title: 'Nexus 9',
+        price: 399.95,
+        image: 'img/nexus9.jpg'
+    }, {
+        title: 'NVidia Shield',
+        price: 299.95,
+        image: 'img/shield.jpg'
+    }
+];
